7eae14118af2549d3c5502f644a6f830c6cff8a1
葛 智紀 <tomoki.katsu@kddi-web.com>
2021-01-08 16:17:40 +0900

    send code use sms

--- code/app/Http/Controllers/VerificationsController.php
+++ code/app/Http/Controllers/VerificationsController.php
@@ -3,21 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Verify;
+use Twilio\Rest\Client;
 
 class VerificationsController extends Controller
 {
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // 認証コードの送信
     public function verifications(Request $request)
     {
-        return response()->json([
-            'message' => 'send success'
-        ]);
+
+        // 送信済確認
+        $verifies = Verify::where('to',$request->to)->whereNotIn('status',['verified'])->get();
+
+        $verify;
+        $code;
+        if(count($verifies) == 0) {
+            // 未送信
+            $code = $this->generateCode(6);
+            $verify = new Verify;
+            $verify->to = $request->to;
+            $verify->code = $code;
+        } else {
+            // 送信済
+            $verify = $verifies->first();
+            $code = $verify->code;
+        }
+
+        $result = $this->sendCode($code,$request->channel,$request->to);
+        
+        if($result) {
+            $verify->status = 'send';
+            $verify->save();
+            return response()->json([
+                'message' => 'send success'
+            ],200);
+        } else {
+            return response()->json([
+                'message' => 'send success'
+            ],400);
+        }
     }
 
     /**
@@ -32,5 +57,32 @@ class VerificationsController extends Controller
         return response()->json([
             'message' => 'verification success'
         ]);
+    }
+
+    // 認証コード生成
+    private function generateCode($length)
+    {
+        $max = pow(10, $length) - 1;
+        $rand = random_int(0, $max);
+        $code = sprintf('%0'. $length. 'd', $rand);
+
+        return $code;
+    }
+
+    // 認証コード送信
+    private function sendCode($code,$channel,$to) {
+        $client = new Client(env('TWILIO_SID'),env('TWILIO_TOKEN'));
+        if($channel == 'sms') {
+            $client->messages->create($to,
+            [
+                'from' => env( 'TWILIO_FROM' ),
+                'body' => 'あなたの認証コードは '.$code.' です。',
+            ]);
+            return true;
+        } else if($channel == 'call'){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
